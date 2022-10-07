ff0e6c4080119a3cf19a791ab1e020c043a3cea5
vukona <vukona@mlab.co.za>
2022-10-07 11:45:25 +0200

    added price, image and name, commit by vhuthu

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 const puppeteer = require("puppeteer");
+const cors=require('cors')
 const express = require('express')
 const app = express();
 const port = 5000;
 
-
+app.use(cors());
 async function getUrls(search) {
   const browser = await puppeteer.launch({
     // headless: false,
@@ -11,15 +12,23 @@ async function getUrls(search) {
   });
   const url =   "https://www.game.co.za/l/search/?t=";
   const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(60000);
+  await page.setDefaultNavigationTimeout(90000);
   console.log(url + search);
   await page.goto(url + search);
   await page.waitForSelector("#react-app");
   const titles = await page.evaluate(() => {
-    const selector = "#react-app > div > div > div > div > div > div > div.css-1dbjc4n.r-13awgt0 > div > div.css-1dbjc4n.r-1p0dtai.r-6koalj.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-13awgt0.r-12vffkv > div > div > div > div.css-1dbjc4n.r-13awgt0 > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-13awgt0.r-18u37iz.r-13qz1uu > div.css-1dbjc4n.r-13awgt0 > div.css-1dbjc4n.r-150rngu.r-eqz5dr.r-16y2uox.r-1wbh5a2.r-1mnahxq.r-11yh6sk.r-1rnoaur.r-1sncvnh > div > div > div > div > div"
+    const selector =      "#react-app > div > div > div > div > div > div > div.css-1dbjc4n.r-13awgt0 > div.css-1dbjc4n.r-1p0dtai.r-6koalj.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-1p0dtai.r-6koalj.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-13awgt0.r-12vffkv > div > div > div > div.css-1dbjc4n.r-13awgt0 > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-13awgt0.r-18u37iz.r-13qz1uu > div.css-1dbjc4n.r-13awgt0 > div.css-1dbjc4n.r-150rngu.r-eqz5dr.r-16y2uox.r-1wbh5a2.r-1mnahxq.r-11yh6sk.r-1rnoaur.r-1sncvnh > div > div > div > div > div"
+    const titleSelector = "a:nth-child(5)> div"
+    const selector2 = " div.css-901oao.r-g3a1by.r-19aw4kv.r-1i10wst.r-vw2c0b.r-knv0ih"
     const data = Array.from(document.querySelectorAll(selector)).map(el => {
-      const title = el.querySelector('a').getAttribute('href');
-      return title
+      // const title = el.querySelector('a').getAttribute('href');
+      const image= el.querySelector('div > div > div > img').getAttribute('src')
+      const  price = el.querySelector(selector2).textContent
+      const  name = el.querySelector(titleSelector).textContent
+      // console.log(el.jsonValue())
+  
+       
+      return { price, name, image}
     })
     return data
   })
